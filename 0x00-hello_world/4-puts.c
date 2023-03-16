bb5e5f7614355bc6450c5b86cdc3f700ce0c2a28
thandeka2020 <125558831+thandeka2020@users.noreply.github.com>
2023-03-16 17:51:59 +0200

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
  * main - main entry point 
- * return : always 0 (success)
+ * return : always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle,\n")
 	return (0);
 }
